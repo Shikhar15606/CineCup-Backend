318cbe911f7aa19de3849f87b8adb1025dbc5112
Shikhar15606 <shikharshukla15606@gmail.com>
2020-12-28 18:42:08 +0530

    Deploy Manage

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const nodemailer = require('nodemailer');
 const key = require('./key/key')
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
---
+++ key/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+    SENDER_EMAIL:process.env.MONGO_URI,
+    SENDER_EMAIL_PASSWORD:process.env.SENDER_EMAIL_PASSWORD
+}
