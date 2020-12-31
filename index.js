294580e01ce0dc9c782d0a47e2e20008bf0f5a21
Shikhar15606 <shikharshukla15606@gmail.com>
2020-12-31 19:32:16 +0530

    Back to silly bug fix

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ algorithms: ['RS256']
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://cinecup-9b0ac.web.app"); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
