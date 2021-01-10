2143d1976d9d0c0982c03451d4ffd55b32e0bbd7
Shikhar15606 <shikharshukla15606@gmail.com>
2021-01-10 13:42:37 +0530

    Removed AUTH0

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const cors = require('cors');
 const nodemailer = require('nodemailer');
 const key = require('./key/key');
 
-var jwt = require('express-jwt');
-var jwks = require('jwks-rsa');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,50 +12,39 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
-// var whitelist = ['https://cinecup-9b0ac.web.app']
+var whitelist = ['https://cinecup-9b0ac.web.app']
 
-// var corsOptions = {
-//   origin: function (origin, callback) {
-//     console.log(origin);
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   },
-//   optionsSuccessStatus: 200, 
-//   methods: "POST"
-// }
+var corsOptions = {
+  origin: function (origin, callback) {
+    console.log(origin);
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  optionsSuccessStatus: 200, 
+  methods: "POST"
+}
 
-var jwtCheck = jwt({
-  secret: jwks.expressJwtSecret({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 5,
-      jwksUri: 'https://dev-3yqn-gsx.au.auth0.com/.well-known/jwks.json'
-}),
-audience: 'https://cinecup-backend.herokuapp.com',
-issuer: 'https://dev-3yqn-gsx.au.auth0.com/',
-algorithms: ['RS256']
-});
 
-app.options('*', cors());
+// app.options('*', cors());
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "https://cinecup-9b0ac.web.app");
+    res.header('Access-Control-Allow-Origin', "https://cinecup-9b0ac.web.app");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
+    next();
 });
 
-app.use(jwtCheck);
+app.use(cors(corsOptions))
 
-app.get('/',cors(),(req,res) => {
+app.get('/',(req,res) => {
   res.send({working:"True"})
 })
 
-app.post('/send',cors() ,(req, res) => {
+app.post('/send',(req, res) => {
     const output = `
       <p>
       The movie ${req.body.movieName} has been blacklisted.
