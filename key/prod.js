dfe0a8dd460f4d138849ffa50e42a005de5ea23b
Shikhar15606 <shikharshukla15606@gmail.com>
2021-05-24 14:31:50 +0530

    Updated ReadMe

--- key/prod.js
+++ key/prod.js
@@ -1,9 +1,6 @@
 module.exports = {
   SENDER_EMAIL: process.env.SENDER_EMAIL,
   SENDER_EMAIL_PASSWORD: process.env.SENDER_EMAIL_PASSWORD,
-  USERNAME: process.env.USERNAME,
-  PASSWORD: process.env.PASSWORD,
-  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
   ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN,
   SERVICEACCOUNT: process.env.SERVICEACCOUNT,
   type: process.env.type,
